abf57e8b4086c8cef850030cc784fefba581e823
mauricio23 <anmauri93@hotmail.com>
2026-04-16 20:10:04 -0500

    proyecto 2 paso 3 mejorado

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,17 +1,24 @@
 import argparse 
 import json
 
+def definir_rutas():
+    ruta_1 = r'C:\Users\MAURICIO\Documents\GitHub\python-project-174\file1.json'
+    data_1 = json.load(open(ruta_1))
+    print (data_1)
+    
+    ruta_2 = r'C:\Users\MAURICIO\Documents\GitHub\python-project-174\file2.json'
+    data_2 = json.load(open(ruta_2))
+    print (data_2)
+
+    return ruta_1, ruta_2
+
+
 def main():
    parser = argparse.ArgumentParser(
      description="Compares two configuration files and shows a difference."
- )
-   ruta_1 = r'C:\Users\MAURICIO\Documents\GitHub\python-project-174\file1.json'
-   data_1 = json.load(open(ruta_1))
-   print (data_1)
-   
-   ruta_2 = r'C:\Users\MAURICIO\Documents\GitHub\python-project-174\file2.json'
-   data_2 = json.load(open(ruta_2))
-   print (data_2)
+     )
+   ruta_1, ruta_2 = definir_rutas
+  
 
    parser.add_argument(ruta_1, help="First file to compare")
    parser.add_argument(ruta_2, help="Second file to compare")
