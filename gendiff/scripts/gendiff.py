4c4d6d8037469bf7e9e37ecde72958c8016c3a13
mauricio23 <anmauri93@hotmail.com>
2026-04-16 20:01:00 -0500

    proyecto 2 paso 3

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,23 @@
-import argparse
+import argparse 
+import json
 
 def main():
- parser = argparse.ArgumentParser(
+   parser = argparse.ArgumentParser(
      description="Compares two configuration files and shows a difference."
  )
- parser.add_argument("first_file", help="First file to compare")
- parser.add_argument("second_file", help="Second file to compare")
+   ruta_1 = r'C:\Users\MAURICIO\Documents\GitHub\python-project-174\file1.json'
+   data_1 = json.load(open(ruta_1))
+   print (data_1)
+   
+   ruta_2 = r'C:\Users\MAURICIO\Documents\GitHub\python-project-174\file2.json'
+   data_2 = json.load(open(ruta_2))
+   print (data_2)
 
- args = parser.parse_args()
-
+   parser.add_argument(ruta_1, help="First file to compare")
+   parser.add_argument(ruta_2, help="Second file to compare")
+   
+   args = parser.parse_args()
+   
+   
 if __name__ == "__main__":
- main()
+    main()
